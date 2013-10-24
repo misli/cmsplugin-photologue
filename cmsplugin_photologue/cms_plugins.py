e4a937ca3b6531846fdf8f629e91a3acab89fac7
Jakub Dorňák <jdornak@redhat.com>
2013-10-24 15:21:28 +0200

    Initial version

---
+++ cmsplugin_photologue/cms_plugins.py
@@ -0,0 +1,45 @@
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+
+from photologue.models import Photo, Gallery
+from cmsplugin_photologue.models import PhotologuePhoto, PhotologueGallery
+
+from django.conf import settings
+from django.utils.translation import ugettext as _
+
+
+class PhotologuePhotoPlugin(CMSPluginBase):
+    model = PhotologuePhoto
+    name = _('Photologue Photo')
+    text_enabled = True
+    fields = ('obj', 'template', 'display_size', 'link_size', 'display_link')
+
+    def render(self, context, instance, placeholder):
+        context.update({
+            'plugin': instance,
+            'photo': instance.photo,
+            'placeholder': placeholder,
+        })
+        return context
+
+    def icon_src(self, instance):
+        return instance.photo.plugin_display_url
+
+
+class PhotologueGalleryPlugin(CMSPluginBase):
+    model = PhotologueGallery
+    name = _('Photologue Gallery')
+    fields = ('obj', 'template', 'display_size', 'link_size', 'display_link', 'order', 'limit')
+
+    def render(self, context, instance, placeholder):
+        context.update({
+            'plugin': instance,
+            'gallery': instance.gallery,
+            'placeholder': placeholder,
+        })
+        return context
+
+
+plugin_pool.register_plugin(PhotologueGalleryPlugin)
+plugin_pool.register_plugin(PhotologuePhotoPlugin)
+
---
+++ cmsplugin_photologue/models.py
@@ -0,0 +1,140 @@
+from django.db import models
+from django.conf import settings
+from django.utils.functional import curry
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse
+
+from cms.models import CMSPlugin
+from photologue.models import Gallery, Photo, PhotoSize
+
+import random
+
+
+# use CMSPLUGIN_PHOTOLOGUE_PHOTO_TEMPLATES to override PHOTO_TEMPLATES
+# or CMSPLUGIN_PHOTOLOGUE_ADD_PHOTO_TEMPLATES to add custom PHOTO_TEMPLATES
+# to the existing ones
+
+PHOTO_TEMPLATES = getattr(settings, 'CMSPLUGIN_PHOTOLOGUE_PHOTO_TEMPLATES', (
+    ('default', _('default')),
+    ('center',  _('center')),
+    ('left',    _('left')),
+    ('right',   _('right')),
+)) + getattr(settings, 'CMSPLUGIN_PHOTOLOGUE_ADD_PHOTO_TEMPLATES', ())
+
+# use CMSPLUGIN_PHOTOLOGUE_GALLERY_TEMPLATES to override GALLERY_TEMPLATES
+# or CMSPLUGIN_PHOTOLOGUE_ADD_GALLERY_TEMPLATES to add custom GALLERY_TEMPLATES
+# to the existing ones
+
+GALLERY_TEMPLATES = getattr(settings, 'CMSPLUGIN_PHOTOLOGUE_GALLERY_TEMPLATES', (
+    ('default', _('default')),
+)) + getattr(settings, 'CMSPLUGIN_PHOTOLOGUE_ADD_GALLERY_TEMPLATES', ())
+
+
+ORDER_CHOICES = (
+    ('gallery', _('Gallery Order')),
+    ('latest',  _('Latest first')),
+    ('oldest',  _('Oldest first')),
+    ('random',  _('Random order')),
+)
+
+
+class PhotologuePlugin(CMSPlugin):
+    display_size = models.ForeignKey(PhotoSize, related_name='+', blank=True, null=True)
+    link_size    = models.ForeignKey(PhotoSize, related_name='+', blank=True, null=True)
+    display_link = models.BooleanField(_('display link'))
+
+    def _add_size_attributes(self, photo):
+        if self.display_size:
+            setattr(photo, 'plugin_display_url',  photo._get_SIZE_url(size=self.display_size.name))
+            setattr(photo, 'plugin_display_size', photo._get_SIZE_size(size=self.display_size.name))
+        else:
+            setattr(photo, 'plugin_display_url',  photo.image.url)
+            setattr(photo, 'plugin_display_size', (photo.image.width, photo.image.height))
+        if self.link_size:
+            setattr(photo, 'plugin_link_url',  photo._get_SIZE_url(size=self.link_size.name))
+            setattr(photo, 'plugin_link_size', photo._get_SIZE_size(size=self.link_size.name))
+        else:
+            setattr(photo, 'plugin_link_url',  photo.image.url)
+            setattr(photo, 'plugin_link_size', (photo.image.width, photo.image.height))
+        return photo
+
+    def copy_relations(self, oldinstance):
+        self.obj          = oldinstance.obj
+        self.display_size = oldinstance.display_size
+        self.link_size    = oldinstance.link_size
+
+    def __unicode__(self):
+        if self.obj:
+            return unicode(self.obj)
+        else:
+            return _('all')
+
+    @property
+    def title(self):
+        return self.obj.title
+
+    @property
+    def title_slug(self):
+        return self.obj.title_slug
+
+    class Meta:
+        abstract = True
+
+
+
+class PhotologuePhoto(PhotologuePlugin):
+    obj = models.ForeignKey(Photo, verbose_name=_('photo'))
+    template     = models.CharField(_('template'), max_length=100, choices=PHOTO_TEMPLATES,
+                                default=PHOTO_TEMPLATES[0][0],
+                                help_text=_('The template used to render the image.'))
+
+    @property
+    def render_template(self):
+        return 'cmsplugin_photologue/photo-%s.html' % self.template
+
+    @property
+    def photo(self):
+        return self._add_size_attributes(self.obj)
+
+
+class PhotologueGallery(PhotologuePlugin):
+    obj = models.ForeignKey(Gallery, verbose_name=_('gallery'), blank=True, null=True)
+    order = models.CharField(_('order'), max_length=8, default='gallery', choices=ORDER_CHOICES)
+    limit = models.PositiveIntegerField(default=0, help_text=u'0 means no limit')
+    template = models.CharField(_("template"), max_length=100, choices=GALLERY_TEMPLATES,
+                                default=GALLERY_TEMPLATES[0][0],
+                                help_text=_('The template used to render the gallery.'))
+
+    @property
+    def render_template(self):
+        return 'cmsplugin_photologue/gallery-%s.html' % self.template
+
+    @property
+    def gallery(self):
+        return self.obj
+
+    @property
+    def photos(self):
+        if self.obj:
+            photo_set = self.obj.photos
+        else:
+            photo_set = Photo.objects
+        photo_set = photo_set.filter(is_public=True)
+        if self.order == 'latest':
+            photo_set = photo_set.order_by('-date_added', '-id')
+        elif self.order == 'oldest':
+            photo_set = photo_set.order_by('date_added', 'id')
+        if self.limit:
+            photos = list(photo_set[:self.limit])
+        else:
+            photos = list(photo_set)
+        if self.order == 'random':
+            random.shuffle(photos)
+        return map(self._add_size_attributes, photos)
+
+    def get_breadcrumb(self):
+        breadcrumb = super(PhotologueGallery, self).get_breadcrumb()
+        if self.obj:
+            breadcrumb.append({'url': reverse('admin:photologue_gallery_change', args=[self.obj.id]), 'title': unicode(self.obj)})
+        return breadcrumb
+
