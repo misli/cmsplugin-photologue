c27e25740a681985f57a8f45a3d285447de3f8e4
Jakub Dorňák <jakub.dornak@misli.cz>
2019-06-04 16:00:40 +0200

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name            = 'cmsplugin-photologue',
-    version         = '1.1.0',
+    version         = '1.1.1',
     description     = 'django CMS plugin for including photos and galleries from Photologue',
     author          = 'Jakub Dorňák',
     author_email    = 'jakub.dornak@misli.cz',
